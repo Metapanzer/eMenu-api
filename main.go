79c20fbe8282ba6130a2e04a1b337a4581b09128
Krisna Sandy Pribadi <113227273+Metapanzer@users.noreply.github.com>
2023-08-28 03:02:35 +0700

    Update main.go

--- main.go
+++ main.go
@@ -31,7 +31,9 @@ func main() {
 	docs.SwaggerInfo.Title = "eMenu API"
 	docs.SwaggerInfo.Description = "This is a sample of eMenu API."
 	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = utils.Getenv("API_URL", "fp-sanbercode-go-48-krisna-production.up.railway.app")
 	docs.SwaggerInfo.BasePath = "/api/v1"
+	docs.SwaggerInfo.Schemes = []string{"http", "https"}
 
 	//Connect to database
 	db := config.ConnectDataBase()
